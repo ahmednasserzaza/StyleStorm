953a1e7e1e0bd1d0d2484b1bee952f1e4a20e2de
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-04-14 17:32:28 +0200

    implement LaundryAdapter.kt

--- app/src/main/java/com/fighter/stylestorm/ui/laundry/LaundryAdapter.kt
+++ app/src/main/java/com/fighter/stylestorm/ui/laundry/LaundryAdapter.kt
@@ -1,6 +1,28 @@
 package com.fighter.stylestorm.ui.laundry
 
-class LaundryAdapter(private val dirtyClothes:List<Int>) {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.fighter.stylestorm.databinding.ItemLaundryBinding
 
-    inner class LaundryViewHolder()
+class LaundryAdapter(private val dirtyClothes:List<Int>):RecyclerView.Adapter<LaundryAdapter.LaundryViewHolder>() {
+
+    inner class LaundryViewHolder(val binding: ItemLaundryBinding):ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LaundryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemLaundryBinding.inflate(inflater , parent , false)
+        return LaundryViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = dirtyClothes.size
+
+    override fun onBindViewHolder(holder: LaundryViewHolder, position: Int) {
+        val currentItem = dirtyClothes[position]
+        Glide.with(holder.binding.root)
+            .load(currentItem)
+            .into(holder.binding.imageLaundryItem)
+    }
 }
