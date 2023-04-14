1ef61c577dc0b750127430917d421b905611c6dc
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-04-14 06:19:45 +0200

    add list of summer items an winter

--- app/src/main/java/com/fighter/stylestorm/data/DataManager.kt
+++ app/src/main/java/com/fighter/stylestorm/data/DataManager.kt
@@ -1,4 +1,23 @@
 package com.fighter.stylestorm.data
 
-object DataManager {
+import com.fighter.stylestorm.R
+
+class DataManager {
+    private val summerClothes = listOf(
+        R.drawable.summer,
+        R.drawable.summer1,
+        R.drawable.summer2,
+        R.drawable.summer3,
+        R.drawable.summer4,
+        R.drawable.summer5,
+        R.drawable.summer6
+    )
+    private val winterClothes = listOf(
+        R.drawable.winter1,
+        R.drawable.winter2,
+        R.drawable.winter3,
+        R.drawable.winter4,
+        R.drawable.winter5,
+        R.drawable.winter6
+    )
 }
